80e6d9e8f6152426beace9f434669cab51637fb2
fischer8 <fischxr@hotmail.com>
2023-10-04 10:00:48 -0300

    nvim

--- after/plugin/snippets.lua
+++ after/plugin/snippets.lua
@@ -1,3 +1,4 @@
+-- "-1read" is a native vim functionality see https://www.youtube.com/watch?v=XA2WjJbmmoM&t=2723s
 vim.keymap.set("n", ",jsx",":-1read ~/.config/nvim/snippets/react-start.txt<CR>2jf(i" ,{ noremap = true})
 vim.keymap.set("n", ",fn",":-1read ~/.config/nvim/snippets/function.txt<CR>f(i" ,{ noremap = true})
 
